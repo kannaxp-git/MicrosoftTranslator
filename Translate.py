d4c73f5fc27d7d82c522268f5a84d3e9a727c38f
Kannan Chandrasekaran <kach@microsoft.com>
2020-08-17 14:47:02 -0700

    Update Translate.py

--- Translate.py
+++ Translate.py
@@ -7,6 +7,9 @@
 # You may need to install requests and uuid.
 # Run: pip install requests uuid
 
+#Setting environment variable (powershell command): setx COGNITIVE_SERVICE_KEY "your-key"
+
+
 import os, requests, uuid, json
 
 key_var_name = 'TRANSLATOR_TEXT_SUBSCRIPTION_KEY'
@@ -22,20 +25,23 @@ endpoint = os.environ[endpoint_var_name]
 # If you encounter any issues with the base_url or path, make sure
 # that you are using the latest endpoint: https://docs.microsoft.com/azure/cognitive-services/translator/reference/v3-0-translate
 path = '/translate?api-version=3.0'
-params = '&to=de&to=it'
+params = '&to=en'
+#params = '&to=de&to=it'
 constructed_url = endpoint + path + params
 
 headers = {
     'Ocp-Apim-Subscription-Key': subscription_key,
+    'Ocp-Apim-Subscription-Region': 'centralindia',
     'Content-type': 'application/json',
     'X-ClientTraceId': str(uuid.uuid4())
 }
 
 # You can pass more than one object in body.
 body = [{
-    'text' : 'Hello World!'
+    'text' : '株式会社 シバタ'
 }]
 request = requests.post(constructed_url, headers=headers, json=body)
 response = request.json()
 
+print(body)
 print(json.dumps(response, sort_keys=True, indent=4, separators=(',', ': ')))
